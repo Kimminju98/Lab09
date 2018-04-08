ea38856abf91f3cb477427b157f148ba9b4e65bc
Kimminju98 <21700097@handong.edu>
2018-04-08 23:15:47 +0900

    first SaleReporter and SalesMen class

---
+++ src/main/java/handong/edu/cess/java/sale/SalesMen.java
@@ -0,0 +1,36 @@
+package handong.edu.cess.java.sale;
+
+public class SalesMen {
+
+	private String name_m;
+	private double sales;
+	private double distance;
+	private String compare;
+
+	public String getCompare() {
+		return compare;
+	}
+	public void setCompare(String compare) {
+		this.compare = compare;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public String getName() {	
+		return name_m;
+	}
+	public void setName(String name_m) {
+		this.name_m = name_m;
+	}
+	public double getSales() {
+		return sales;
+	}
+	public void setSales(double sales) {
+		this.sales = sales;
+	}
+
+
+}
